d64a830a05f53aff94bb2bbfaee52b4799dea2be
Helgen97 <drmegman@gmail.com>
2022-06-21 10:19:45 +0300

    Some adding for google search #8

--- src/main/java/com/diploma/forum/controllers/ReactAppController.java
+++ src/main/java/com/diploma/forum/controllers/ReactAppController.java
@@ -13,16 +13,66 @@ public class ReactAppController implements ErrorController {
         return "index";
     }
 
+//    Static files endpoints
+
     @RequestMapping("robots.txt")
     public String getRobots() {
 
         return "robots.txt";
     }
 
-    @RequestMapping("sitemap.xml")
+    @RequestMapping("sitemap.txt")
     public String getSiteMap() {
 
-        return "sitemap.xml";
+        return "sitemap.txt";
+    }
+
+    @RequestMapping("favicon.ico")
+    public String getFaviconIco() {
+
+        return "favicon.ico";
+    }
+
+    @RequestMapping("favicon.png")
+    public String getFaviconPng() {
+
+        return "favicon.png";
+    }
+
+    @RequestMapping("apple-touch-icon-precomposed")
+    public String getAppleTouchIconPrecomposed() {
+
+        return "apple-touch-icon-precomposed.png";
+    }
+
+    @RequestMapping("apple-touch-icon-180x180-precomposed")
+    public String getAppleTouchIcon180x180Precomposed() {
+
+        return "apple-touch-icon-180x180-precomposed.png";
+    }
+
+    @RequestMapping("apple-touch-icon-152x152-precomposed")
+    public String getAppleTouchIcon152x152Precomposed() {
+
+        return "apple-touch-icon-152x152-precomposed.png";
+    }
+
+    @RequestMapping("apple-touch-icon-144x144-precomposed")
+    public String getAppleTouchIcon144x144Precomposed() {
+
+        return "apple-touch-icon-144x144-precomposed.png";
+    }
+
+    @RequestMapping("apple-touch-icon-114x114-precomposed")
+    public String getAppleTouchIcon114x114Precomposed() {
+
+        return "apple-touch-icon-114x114-precomposed.png";
+    }
+
+    @RequestMapping("apple-touch-icon-72x72-precomposed")
+    public String getAppleTouchIcon72x72Precomposed() {
+
+        return "apple-touch-icon-72x72-precomposed.png";
     }
 
 }
